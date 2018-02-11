5778b2fdd18447a29fe206499f931b2811d8e010
Jinghan Cao <caojinghan123@hotmail.com>
2018-02-11 23:34:59 +0800

    Add files via upload

---
+++ Exercise_10.05.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+#include<list>
+#include<string>
+#include<utility>
+using namespace std;
+/*
+Exercise
+10.5:
+定义一个 map 对象，将单词与一个 list 对象关联起
+来，该 list 对象存储对应的单词可能的行号
+*/
+
+int main(){
+	map<string, list<int> > iMap;
+	return 0;
+}
